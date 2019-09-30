e32f2e0c40a42b72632e2c64127396858b6bd656
Ekrynox <lucas123.a17@gmail.com>
2019-09-30 17:55:09 +0200

    Open Register activity when click on "Create an account"

--- app/src/main/java/com/example/projetmobile4a/LoginActivity.kt
+++ app/src/main/java/com/example/projetmobile4a/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.projetmobile4a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun buttonRegisterOnClick(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java).apply {}
+        startActivity(intent)
     }
 }
