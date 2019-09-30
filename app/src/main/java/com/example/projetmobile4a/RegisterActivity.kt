9e81628878cfa29205e4002783244dc442409f3e
Ekrynox <lucas123.a17@gmail.com>
2019-09-30 17:54:02 +0200

    Add Register Activity

---
+++ app/src/main/java/com/example/projetmobile4a/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.example.projetmobile4a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun buttonRegisterOnClick(view: View) {
+
+    }
+}
