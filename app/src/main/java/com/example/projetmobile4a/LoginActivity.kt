da9172ca0154cad162a567ad00c741aa0a4556bd
Ekrynox <lucas123.a17@gmail.com>
2019-12-10 17:10:43 +0100

    Start to implement SharedPreference for login

--- app/src/main/java/com/example/projetmobile4a/LoginActivity.kt
+++ app/src/main/java/com/example/projetmobile4a/LoginActivity.kt
@@ -1,21 +1,32 @@
 package com.example.projetmobile4a
 
-import com.example.projetmobile4a.controller.Rest
-import com.example.projetmobile4a.model.RestUser
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetmobile4a.controller.Rest
+import com.example.projetmobile4a.model.RestUser
+
 
 class LoginActivity : AppCompatActivity() {
-    private var api : Rest? = null
+    private var api: Rest? = null
+    private var pref: SharedPreferences? = null
+
+    private var email = ""
+    private var password = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        pref =  getPreferences(Context.MODE_PRIVATE)
+        email = pref?.getString("email", "")!!
+        password = pref?.getString("password", "")!!
+        api?.login(this::loginResponse, null, email, password)
 
         api = Rest.getInstance()
     }
@@ -26,19 +37,30 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun buttonLoginOnClick(view: View) {
-        api?.login(this::loginResponse, null, findViewById<EditText>(R.id.textinput_email).text.toString(), findViewById<EditText>(R.id.textinput_password).text.toString())
+        email = findViewById<EditText>(R.id.textinput_email).text.toString()
+        password = findViewById<EditText>(R.id.textinput_password).text.toString()
+        api?.login(this::loginResponse, null, email, password)
     }
 
     private fun loginResponse(data: RestUser) {
         if (data.error == null) {
-            Log.d(this.toString(), data.pseudo)
+            val editor = pref?.edit()
+            editor?.putString("email", email)
+            editor?.apply()
+            editor?.commit()
+            editor?.putString("password", password)
+            editor?.apply()
+            editor?.commit()
 
             val intent = Intent(this, MainActivity::class.java).apply {}
             startActivity(intent)
             finish()
             return
+        } else if (data.error == "already_logged") {
+            val intent = Intent(this, MainActivity::class.java).apply {}
+            startActivity(intent)
+            finish()
+            return
         }
-
-        Log.d(this.toString(), data.error)
     }
 }
