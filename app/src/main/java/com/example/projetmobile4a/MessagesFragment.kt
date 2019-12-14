ee6d7c589060603230bf8eab5c63118e49131c21
Ekrynox <lucas123.a17@gmail.com>
2019-12-14 16:49:09 +0100

    Change RecyclerView creation Process (Cleaner)

--- app/src/main/java/com/example/projetmobile4a/MessagesFragment.kt
+++ app/src/main/java/com/example/projetmobile4a/MessagesFragment.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetmobile4a.controller.Rest
+import com.example.projetmobile4a.model.RestUser
 import com.example.projetmobile4a.model.RestUsersList
 import kotlinx.android.synthetic.main.fragment_messages.*
 
@@ -19,49 +20,49 @@ class MessagesFragment : Fragment() {
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    private var users: List<RestUser> = ArrayList()
+    private var friends: List<RestUser> = ArrayList()
+
     private var userId = 0
     private var userPseudo = ""
 
     private var api: Rest = Rest.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_messages, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
         userId = arguments?.getInt("USER_ID") ?: 0
         userPseudo = arguments?.getString("USER_PSEUDO") ?: ""
 
-        api.getDiscussions(fun (users: RestUsersList) {
-            api.getFriends(fun (friends: RestUsersList) {
-                if (users.error == null) {
-                    viewManager = LinearLayoutManager(activity)
-                    viewAdapter = UsersListAdapter(users.users!!, friends.users!!, this::updateUserList, userId)
-                    recyclerView = my_recycler_view.apply {
-                        setHasFixedSize(true)
-                        layoutManager = viewManager
-                        adapter = viewAdapter
-                    }
+        viewManager = LinearLayoutManager(activity)
+        viewAdapter = UsersListAdapter(users, friends, this::updateUserList, userId)
+        recyclerView = my_recycler_view.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
 
-                    val dividerItemDecoration = DividerItemDecoration(
-                        my_recycler_view.context,
-                        (viewManager as LinearLayoutManager).orientation
-                    )
-                    my_recycler_view.addItemDecoration(dividerItemDecoration)
+        val dividerItemDecoration = DividerItemDecoration(my_recycler_view.context,  (viewManager as LinearLayoutManager).orientation)
+        my_recycler_view.addItemDecoration(dividerItemDecoration)
 
-                    return
-                }
-            }, null)
-        }, null)
-
-        return inflater.inflate(R.layout.fragment_messages, container, false)
+        updateUserList()
     }
 
     private fun updateUserList() {
         api.getDiscussions(fun (users: RestUsersList) {
-            api.getFriends(fun (friends: RestUsersList) {
-                if (users.error == null) {
-                    viewAdapter = UsersListAdapter(users.users!!, friends.users!!, this::updateUserList, userId)
-                    recyclerView.swapAdapter(viewAdapter, false)
-                    return
-                }
-            }, null)
+            if (users.error == null) {
+                api.getFriends(fun(friends: RestUsersList) {
+                    if (friends.error == null) {
+                        this.users = users.users!!
+                        this.friends = friends.users!!
+                        recyclerView.swapAdapter( UsersListAdapter(this.users, this.friends, this::updateUserList, userId), true)
+                    }
+                }, null)
+            }
         }, null)
     }
 }
